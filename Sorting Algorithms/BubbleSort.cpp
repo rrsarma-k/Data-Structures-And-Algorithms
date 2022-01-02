0a00c966678313dd2458100cfa85640499cccaa0
rrsarma0 <rrsarma.kambhampati@gmail.com>
2022-01-02 16:32:37 +0530

    updated few sorting implementations

---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+// Complexity: O(n^2).
+void sort1(int* ar, int n) {
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n - 1; ++j) {
+            if(ar[j] > ar[j + 1]) swap(ar[j], ar[j + 1]);
+        }
+    }
+}
+
+// If we observe carefully in sort1, at each pass(for one time outer loop), the last element
+// is the maximum one. So no need to compare those values.
+
+// Complexity: still O(n^2), but optimised.
+void sort2(int* ar, int n) {
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n - 1 - i; ++j) {
+            if(ar[j] > ar[j + 1]) swap(ar[j], ar[j + 1]);
+        }
+    }
+}
+
+// If we observe carefully sometimes array will be sorted at few passes only, like in this ex:
+/*
+    2 7 4 1 3 5
+    pass 1 : {2 4 1 3 5 7}
+    pass 2 : {2 1 3 4 5 7}
+    pass 3 : {1 2 3 4 5 7}
+
+    Here n is 6 but after 3 passes array is sorted. still we're looping 
+    in sort2() even after sorted.
+    Means if the condition in inner loop(ar[j] > ar[j+1]) does not execute at once then array is sorted.
+    Then we can come out of the loop.
+*/
+// Complexity: still O(n^2), but optimised, which results less iterations in best case(even
+// with the array is sorted).
+void sort(int* ar, int n) {
+    for(int i = 0; i < n; ++i) {
+        bool flag = false;
+        for(int j = 0; j < n - 1 - i; ++j) {
+            if(ar[j] > ar[j + 1]) {
+                swap(ar[j], ar[j + 1]);
+                flag = true;
+            }
+        }
+        if(flag == false) break;
+    }
+}
+
+int main() {
+    int n = 6;
+    int ar[] = {2, 7, 4, 1, 3, 5};
+    sort(ar, n);
+    for(int i = 0; i < n; ++i) cout << ar[i] << ' ';
+    cout << '\n';
+}
+
+
+/* 
+** Bubble sort complexity:
+** Best case : O(n)
+** Avg case : O(n^2)
+** Worst case : O(n^2)
+*/
---
+++ Sorting Algorithms/SelectionSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+// Not an inplace sort algo, as it creates an auxilary space(br) w.r.to n.
+void sort1(int* ar, int n) {
+    int br[n];
+    int index = 0;
+    for(int i = 0; i < n; ++i) {
+        int mn_ele_idx = i;
+        for(int j = 0; j < n; ++j) {
+            if(ar[j] < ar[mn_ele_idx]) {
+                mn_ele_idx = j;
+            }
+        }
+        br[index++] = ar[mn_ele_idx];
+        ar[mn_ele_idx] = INT_MAX;
+    }
+    for(int i = 0; i < n; ++i) ar[i] = br[i];
+}
+
+// Inplace sort algo.
+/* Any sorting algorithm outer-loop needs to run only n-1 times,
+   as nth element will already be sorted then. */
+void sort(int* ar, int n) {
+    for(int i = 0; i < n - 1; ++i) {
+        int mn_ele_idx = i;
+        for(int j = i; j < n; ++j) {
+            if(ar[j] < ar[mn_ele_idx]) {
+                mn_ele_idx = j;
+            }
+        }
+        swap(ar[i], ar[mn_ele_idx]);
+    }
+}
+
+int main() {
+    int n = 6;
+    int ar[] = {2, 7, 4, 1, 3, 5};
+    sort(ar, n);
+    for(int i = 0; i < n; ++i) cout << ar[i] << ' ';
+    cout << '\n';
+}
+
+
+/* 
+** Selection sort complexity:
+** Best case : O(n^2)
+** Avg case : O(n^2)
+** Worst case : O(n^2)
+*/
